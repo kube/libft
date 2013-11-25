46d3d85962d24bc76b6f7afd690f8f2438f3be5d
Christophe FEIJOO <cfeijoo@e2r2p10.42.fr>
2013-11-25 21:51:40 +0100

    Added strstr & strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/24 02:14:37 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/11/24 03:05:58 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/11/25 21:49:23 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,25 @@
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	(void)s1;
-	(void)s2;
-	(void)n;
-	return ((char*)s1);
+	size_t		begin;
+	size_t		current;
+
+	begin = 0;
+	while (s1[begin])
+	{
+		ft_putstr("0\n");
+		current = 0;
+		while (s1[begin + current] == s2[current] && current < n)
+		{
+			ft_putstr("a\n");
+			current++;
+		}
+		if (!s2[current])
+		{
+			ft_putstr("b\n");
+			return ((char*)(s1 + begin));
+		}
+		begin++;
+	}
+	return (NULL);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/24 02:09:58 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/11/24 03:28:47 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/11/25 21:33:37 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 
 char	*ft_strstr(const char *s1, const char *s2)
 {
-	(void)s1;
-	(void)s2;
-	return ((char*)s1);
+	int		begin;
+	int		current;
+
+	begin = 0;
+	while (s1[begin])
+	{
+		current = 0;
+		while (s1[begin + current] == s2[current])
+		{
+			current++;
+		}
+		if (!s2[current])
+		{
+			return ((char*)(s1 + begin));
+		}
+		begin++;
+	}
+	return (NULL);
 }
