d6004fb5d2a3701a3b17e00817edf0d588c25488
Christophe FEIJOO <cfeijoo@e2r10p6.42.fr>
2013-11-27 17:05:39 +0100

    Try disable strstr and strnstr for Segfault

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/24 02:14:37 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/11/26 14:40:57 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/11/27 17:04:36 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	size_t		begin;
-	size_t		current;
-
-	begin = 0;
-	while (s1[begin])
-	{
-		current = 0;
-		while (s1[begin + current] == s2[current] && current < n)
-		{
-			current++;
-		}
-		if (!s2[current])
-		{
-			return ((char*)(s1 + begin));
-		}
-		begin++;
-	}
+/*
+ *    size_t		begin;
+ *    size_t		current;
+ *
+ *    begin = 0;
+ *    while (s1[begin])
+ *    {
+ *        current = 0;
+ *        while (s1[begin + current] == s2[current] && current < n)
+ *        {
+ *            current++;
+ *        }
+ *        if (!s2[current])
+ *        {
+ *            return ((char*)(s1 + begin));
+ *        }
+ *        begin++;
+ *    }
+ */
+	(void)s1;
+	(void)s2;
+	(void)n;
 	return (NULL);
 }
