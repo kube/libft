0a97762bc0e811bb11d6cc32b9e88426162a4fe3
Christophe FEIJOO <cfeijoo@e2r13p5.42.fr>
2013-11-29 14:23:59 +0100

    Part 2 almost finished

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,37 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/24 04:42:43 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/11/24 05:14:10 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/11/29 14:23:30 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
+static int	ft_isspace(char	c)
+{
+	if (c == ' ' ||
+		c == '\n' ||
+		c == '\t' ||
+		c == '\v' ||
+		c == '\f' ||
+		c == '\r')
+	{
+		return (1);
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s)
 {
+	unsigned int	i;
+	char			*newstr;
+
+	i = 0;
+	while (s[i])
+		i++;
+	j = ft_strlen(s) - 1;
+	while (!j)
+		j--;
+	newstr = ft_strsub(s, i, j - i);
 	return ((char*)s);
 }
