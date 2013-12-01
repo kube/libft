d3f4df0cca8bf7f5beaf05b16e69dc55ec9cd170
Christophe FEIJOO <cfeijoo@student.42.fr>
2013-12-01 19:30:56 +0100

    Added strmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/01 16:37:01 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/01 16:38:35 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/01 19:29:27 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	(void)*f;
-	return (lst);
+	t_list	*newlst;
+
+	if (lst && *f)
+	{
+		newlst = (*f)(lst);
+		if (newlst && lst->next)
+		{
+			newlst->next = ft_lstmap(lst->next, f);
+		}
+		return (newlst);
+	}
+	return (NULL);
 }
