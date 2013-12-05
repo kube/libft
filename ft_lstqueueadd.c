b873b0aa96e9888e8c741401b2af5cf0e1368925
Christophe FEIJOO <cfeijoo@e2r2p13.42.fr>
2013-12-05 01:50:15 +0100

    test

--- ft_lstqueueadd.c
+++ ft_lstqueueadd.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/04 22:55:28 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/05 00:16:33 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/05 01:49:54 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,11 @@ void	ft_lstqueueadd(t_list **alst, t_list *new)
 		ft_putstr("   *2\n");
 		current = *alst;
 		ft_putstr("   *3\n");
-		while (current->next)
+		while (current && current->next)
+		{
+			ft_putstr("          +A");
 			current = current->next;
+		}
 		ft_putstr("   *4\n");
 		current->next = new;
 		ft_putstr("   *5\n");
