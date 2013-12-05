e0332973506ef8a2d9c958a9b844170d8e8cf509
Christophe FEIJOO <cfeijoo@e2r5p4.42.fr>
2013-12-05 14:56:14 +0100

    test

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/01 15:18:08 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/05 00:14:34 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/05 14:55:47 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*newlst;
-	
-	ft_putstr("   *1\n");
-	newlst = (t_list*)malloc(sizeof(t_list));
-	ft_putstr("   *2\n");
-	newlst->content = ft_memdup(content, content_size);
-	ft_putstr("   *3\n");
-	newlst->content_size = content_size;
-	ft_putstr("   *4\n");
-	newlst->next = NULL;
-	ft_putstr("   *5\n");
+
+		ft_putstr("   *1\n");
+		newlst = (t_list*)malloc(sizeof(t_list));
+		ft_putstr("   *2\n");
+		if (content_size)
+			newlst->content = ft_memdup(content, content_size);
+		ft_putstr("   *3\n");
+		newlst->content_size = content_size;
+		ft_putstr("   *4\n");
+		newlst->next = NULL;
+		ft_putstr("   *5\n");
 	return (newlst);
 }
