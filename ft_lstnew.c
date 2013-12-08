990822c627a68ae8969ca098d4a20342c3561b40
Christophe FEIJOO <cfeijoo@e2r5p4.42.fr>
2013-12-08 22:13:24 +0100

    test

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/01 15:18:08 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/05 20:38:30 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/08 22:13:18 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*newlst;
 
+				ft_putendl("+1a");
 	newlst = (t_list*)malloc(sizeof(t_list));
+				ft_putendl("+1b");
 	if (content_size)
 		newlst->content = ft_memdup(content, content_size);
+				ft_putendl("+2");
 	newlst->content_size = content_size;
 	newlst->next = NULL;
 	return (newlst);
