caf3ad6a62f68fe4bfa45589e70d33eb96e0756a
Christophe FEIJOO <cfeijoo@e2r5p4.42.fr>
2013-12-09 01:01:43 +0100

    test

--- ft_lstqueueadd.c
+++ ft_lstqueueadd.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/04 22:55:28 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/08 22:11:08 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/09 01:01:24 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,16 @@ void	ft_lstqueueadd(t_list **alst, t_list *new)
 {
 	t_list	*current;
 
-				ft_putendl("+0a");
 	if (new && alst)
 	{
-				ft_putendl("+0b");
 		current = *alst;
 		if (current)
 		{
-
-				ft_putendl("+0");
 			while (current->next)
-			{
-				ft_putendl("+1");
 				current = current->next;
-			}
-			ft_putendl("+2");
 			current->next = new;
-			ft_putendl("+3");
 		}
 		else
-		{
-			ft_putendl("+4");
 			ft_lstadd(alst, new);
-		}
 	}
 }
