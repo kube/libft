15f30464d7de8e9df49d77299c8073e67b7ba958
Christophe FEIJOO <cfeijoo@e3r4p4.42.fr>
2013-12-11 14:57:53 +0100

    test

--- ft_lstrev.c
+++ ft_lstrev.c
@@ -6,7 +6,7 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/11 14:27:23 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/11 14:51:11 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/11 14:57:45 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,12 @@ void	ft_lstrev(t_list **alst)
 	t_list	*next;
 
 	current = *alst;
-	ft_putendl("a");
 	while (current)
 	{
-		ft_putendl("b");
 		next = current->next;
-		ft_lstadd(&b, current);
-		ft_putendl("c");
+		current->next = b;
+		b = current;
 		current = next;
-		ft_putendl("d");
 	}
-	ft_putendl("e");
 	alst = &b;
 }
