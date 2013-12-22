fe57690ef14fff02f072ab434fcd494a538360ff
Christophe FEIJOO <cfeijoo@e2r4p4.42.fr>
2013-12-22 15:40:00 +0100

    Optimized bzero again (fixed errors)

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,24 +6,21 @@
 /*   By: cfeijoo <cfeijoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 14:31:22 by cfeijoo           #+#    #+#             */
-/*   Updated: 2013/12/22 15:31:35 by cfeijoo          ###   ########.fr       */
+/*   Updated: 2013/12/22 15:39:11 by cfeijoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void	ft_bzero(void *s, size_t n)
+void		ft_bzero(void *s, size_t n)
 {
-	unsigned int	i;
-	unsigned char	*cache;
+	char	*cache;
 
-	i = 0;
-	cache = (unsigned char*)b;
-	while (i < (unsigned int)len)
+	cache = s;
+	while ((size_t)cache < (size_t)(cache + n))
 	{
-		if (cache[i])
-			cache[i] = c;
-		i++;
+		if (cache)
+			*cache = 0;
+		cache++;
 	}
-	return (b);
 }
